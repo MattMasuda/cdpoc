92dc121f434ec1b9f52d11a83accaa436e814fc9
Matt Masuda <matt.masuda@aa.com>
2015-08-29 11:36:20 -0700

    Fix Jenkins provisioning script

--- jenkins-provision.sh
+++ jenkins-provision.sh
@@ -1,10 +1,9 @@
-#echo "Provision: openjdk"
-#apt-get -q -y install openjdk-7-jdk
-#jenkins has its own jdk 1.6 dependency
-echo "Provision: Jenkins"
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get -q update
+echo "Provision: openjdk"
+apt-get -q -y install openjdk-7-jdk
+echo "Provision: Jenkins"
 apt-get -q -y install jenkins
 
 
